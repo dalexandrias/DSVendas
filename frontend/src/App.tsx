77176bba6a6ca32bd53184f50637d0814c834886
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2021-09-07 13:49:27 -0300

    Update title donut bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
             <BarChart />
           </div>
           <div className='col -sm-6'>
-            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <h5 className="text-center text-secondary">% das Vendas</h5>
             <DonutChart />
           </div>
           <div className="py-3">
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             </div>
         </footer>
 
-    )
+    );
 }
 
 export default Footer;
